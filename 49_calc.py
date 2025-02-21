534f78db7f147a93c598b7d91a0f4738a9bd96e0
rinat iamaev <rinatiamaev@gmail.com>
2025-02-21 22:33:58 +0200

    problem 49 super calc

---
+++ 49_calc.py
@@ -0,0 +1,65 @@
+# Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+
+# Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+ 
+
+# Example 1:
+
+# Input: s = "1 + 1"
+# Output: 2
+# Example 2:
+
+# Input: s = " 2-1 + 2 "
+# Output: 3
+# Example 3:
+
+# Input: s = "(1+(4+5+2)-3)+(6+8)"
+# Output: 23
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 3 * 105
+# s consists of digits, '+', '-', '(', ')', and ' '.
+# s represents a valid expression.
+# '+' is not used as a unary operation (i.e., "+1" and "+(2 + 3)" is invalid).
+# '-' could be used as a unary operation (i.e., "-1" and "-(2 + 3)" is valid).
+# There will be no two consecutive operators in the input.
+# Every number and running calculation will fit in a signed 32-bit integer.
+
+def calculate(s: str) -> int:
+    stack = []
+    current_number = 0
+    result = 0
+    sign = 1
+    
+    for i, char in enumerate(s):
+        if char.isdigit():
+            current_number = current_number * 10 + int(char)
+        
+        elif char == '+':
+            result += sign * current_number
+            current_number = 0
+            sign = 1
+        
+        elif char == '-':
+            result += sign * current_number
+            current_number = 0
+            sign = -1
+        
+        elif char == '(':
+            stack.append(result)
+            stack.append(sign)
+            result = 0
+            sign = 1
+        
+        elif char == ')':
+            result += sign * current_number
+            current_number = 0
+            result *= stack.pop()
+            result += stack.pop()
+
+    result += sign * current_number
+    
+    return result
