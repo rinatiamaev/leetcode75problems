a2359d3749a5ede76875c0fe41539c4e70181db4
rinat iamaev <rinatiamaev@gmail.com>
2025-05-22 20:22:08 +0300

    problem 57 median of two sum

---
+++ 57_MedianTwoSortedArrays.py
@@ -0,0 +1,62 @@
+#  Median of Two Sorted Arrays
+
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+# The overall run time complexity should be O(log (m+n)).
+
+ 
+
+# Example 1:
+
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+# Example 2:
+
+# Input: nums1 = [1,2], nums2 = [3,4]
+# Output: 2.50000
+# Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+# Constraints:
+
+# nums1.length == m
+# nums2.length == n
+# 0 <= m <= 1000
+# 0 <= n <= 1000
+# 1 <= m + n <= 2000
+# -106 <= nums1[i], nums2[i] <= 106
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        # Ensure nums1 is the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        x, y = len(nums1), len(nums2)
+        low, high = 0, x
+
+        while low <= high:
+            partitionX = (low + high) // 2
+            partitionY = (x + y + 1) // 2 - partitionX
+
+            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            minRightX = float('inf') if partitionX == x else nums1[partitionX]
+
+            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minRightY = float('inf') if partitionY == y else nums2[partitionY]
+
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+                if (x + y) % 2 == 0:
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
+                else:
+                    return float(max(maxLeftX, maxLeftY))
+            elif maxLeftX > minRightY:
+                high = partitionX - 1
+            else:
+                low = partitionX + 1
